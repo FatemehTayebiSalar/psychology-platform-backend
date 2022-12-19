4cfcbc620d6680ab8dfe0abc893ae20e4dc8f3c4
Fatemeh TayebiSalar <fatemehtayebi98@gmail.com>
2022-12-19 14:27:04 +0330

    The access level was checked in the admin panel

--- app/http/middlewares/verifyAccessToken.js
+++ app/http/middlewares/verifyAccessToken.js
@@ -3,24 +3,41 @@ const { UserModel } = require("../../models/user");
 const JWT = require("jsonwebtoken");
 const { ACCESS_TOKEN_SECRET_KEY } = require("../../utils/constants");
 
-function veifyAccessToken(req,res,next){
-    const headers = req.headers;
+function getToken(headers){
     const [bearer,token] = headers?.["access-token"]?.split(" ") || [];
-    if(token && ["Bearer" , "bearer"].includes(bearer)){
+    if(token && ["Bearer" , "bearer"].includes(bearer)) return token;
+    throw createError.Unauthorized("حساب کاربری شناسایی نشد وارد حساب کاربری خود شوید");
+}
+function veifyAccessToken(req,res,next){
+    try {
+        const token = getToken(req.headers);
         JWT.verify(token, ACCESS_TOKEN_SECRET_KEY , async (err , payload) => {
-            if(err) return next(createError.Unauthorized("وارد حساب کاربری خود شوید"))
+            if(err) throw createError.Unauthorized("وارد حساب کاربری خود شوید")
             const {mobile} = payload || {};
             const user =  await UserModel.findOne({mobile}, {password:0,otp:0})
-            if(!user) return next(createError.Unauthorized("حساب کاربری یافت نشد"));
+            if(!user) throw createError.Unauthorized("حساب کاربری یافت نشد");
             req.user = user;
             return next();
 
-        }) 
+        }) ;
+    } catch (error) {
+        next(error);
     }
-    else return next(createError.Unauthorized("وارد حساب کاربری خود شوید"))
-   
+}
+
+function checkRole(role){
+    return function (req,res,next){
+        try {
+            const user = req.user;
+            if(user.roles.includes(role)) return next();
+            throw createError.Forbidden("شما به این قسمت دسترسی ندارید");
+        } catch (error) {
+            next(error);
+        }
+    };
 }
 
 module.exports = {
-    veifyAccessToken
+    veifyAccessToken,
+    checkRole
 }
--- app/router/router.js
+++ app/router/router.js
@@ -1,9 +1,10 @@
+const { checkRole, veifyAccessToken } = require("../http/middlewares/verifyAccessToken");
 const { AdminRoutes } = require("./admin/admin.routes");
 const { HomeRoutes } = require("./api");
 const { UserAuthRoutes } = require("./user/auth");
 const router = require("express").Router();
 router.use("/user",UserAuthRoutes)
-router.use("/admin" , AdminRoutes)
+router.use("/admin" ,veifyAccessToken,checkRole("ADMIN"), AdminRoutes)
 router.use("/",HomeRoutes)
 module.exports = {
     AllRoutes : router
