334434d33139a7a8da7e6deb645396e22bb8ed7d
Fatemeh TayebiSalar <fatemehtayebi98@gmail.com>
2022-12-27 21:05:24 +0330

    bug was fixed

--- app/http/validators/admin/video.schema.js
+++ app/http/validators/admin/video.schema.js
@@ -1,8 +1,8 @@
 const Joi = require("@hapi/joi");
 const createError = require("http-errors");
 const createVideoSchema = Joi.object({
-    title : Joi.string().min(3).max(30).error(createError.BadRequest("عنوان پادکست به صورت صحیح وارد نشده است")),
-    coach : Joi.string().min(3).max(30).error(createError.BadRequest("نام راوی پادکست به صورت صحیح وارد نشده است")),
+    title : Joi.string().min(3).max(30).error(createError.BadRequest("عنوان ویدیو به صورت صحیح وارد نشده است")),
+    coach : Joi.string().min(3).max(30).error(createError.BadRequest("نام راوی ویدیو به صورت صحیح وارد نشده است")),
     filename:Joi.string().pattern(/(\.png|\.jpg|\.webp|\.jpeg|\.gif)$/).error(createError.BadRequest("تصویر ارسال شده صحیح نمی باشد")),
     information : Joi.string().error(createError.BadRequest("متن ارسال شده به صورت صحیح وارد نشده است")),
     fileUploadPath:Joi.allow(),
