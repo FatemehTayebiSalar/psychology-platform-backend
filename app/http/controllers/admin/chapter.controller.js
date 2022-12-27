e69af8855a3f91e200e027c35f45f6a29ee9c711
Fatemeh TayebiSalar <fatemehtayebi98@gmail.com>
2022-12-27 21:00:08 +0330

    The chapter section of admin panel was done

--- app/http/controllers/admin/chapter.controller.js
+++ app/http/controllers/admin/chapter.controller.js
@@ -3,6 +3,7 @@ const Controller = require("../contoller");
 const { AdminVideoController } = require("./video.controller");
 const createError = require("http-errors");
 const {StatusCodes : HttpStatus, StatusCodes} = require("http-status-codes");
+const { deleteInvalidData } = require("../../../utils/functions");
 
 class chapterController extends Controller {
     async addChapter(req,res,next){
@@ -22,6 +23,75 @@ class chapterController extends Controller {
         } catch (error) {
             next(error)
         }
+    }
+
+    async getListOfChapters(req,res,next){
+        try {
+            const {videoID} = req.params;
+            const chapters = await this.getChaptersOfVideo(videoID)
+            return res.status(HttpStatus.OK).json({
+                data:{
+                    statusCode : HttpStatus.OK,
+                    chapters
+                }
+            })
+        } catch (error) {
+            next(error)
+        }
+    }
+    async updateChapterById(req,res,next){
+        try {
+            const {chapterID} = req.params;
+            await this.getOneChapter(chapterID);
+            const data = req.body;
+            deleteInvalidData(data ,  ["_id"]);
+            const updateChapterResult = await VideoModel.updateOne({"chapter._id" : chapterID},{
+                $set : {"chapter.$" : data}
+            })
+            if(updateChapterResult.modifiedCount == 0) throw new createError.InternalServerError("به روزرسانی فصل انجام نشد")
+            return res.status(HttpStatus.OK).json({
+                statusCode : HttpStatus.OK,
+                data : {
+                    message : "به روزرسانی با موفقیت انجام شد"
+                }
+            })
+        } catch (error) {
+            next(error)
+        }
+    }
+    async removeChapterById(req,res,next){
+        try {
+            const {chapterID} = req.params;
+            const chapter = await this.getOneChapter(chapterID);
+            const removeResult = await VideoModel.updateOne({"chapter._id" : chapterID} ,{
+                $pull : {
+                    chapter :{
+                        _id : chapterID
+                    }
+                }
+            })
+            if(removeResult.modifiedCount == 0) throw new createError.InternalServerError("حذف فصل انجام نشد")
+            return res.status(HttpStatus.OK).json({
+                statusCode : HttpStatus.OK,
+                data: {
+                    message : "حذف فصل با موفقیت انجام شد"
+                }
+            })
+        } catch (error) {
+            next(error)
+        }
+    }
+
+    async getChaptersOfVideo(id){
+        const chapters = await VideoModel.findOne({_id : id} , {chapter : 1 , title: 1})
+        if (!chapters) throw createError.NotFound("ویدیویی با این شناسه یافت نشد")
+        return chapters;
+    }
+
+    async getOneChapter(id){
+        const chapter = await VideoModel.findOne({"chapter._id" : id},{"chapter.$" : 1})
+        if(!chapter) throw new createError.NotFound("فصلی با این شناسه یافت نشد")
+        return chapter;
     }
 }
 
--- app/router/admin/chapter.js
+++ app/router/admin/chapter.js
@@ -1,7 +1,6 @@
 const { AdminChapterController } = require("../../http/controllers/admin/chapter.controller");
 
 const router = require("express").Router();
-router.put("/add" , AdminChapterController.addChapter)
 
 /**
  * @swagger
@@ -15,7 +14,16 @@ router.put("/add" , AdminChapterController.addChapter)
  *              properties:
  *                  id:
  *                      type: string
- *                      example: 123
+ *                      example: 63a2421363ff2f7f78b655f6
+ *                  title:
+ *                      type: string
+ *                      example: chapter 1 of grief
+ *                  text:
+ *                      type: string
+ *                      example: describe aboute this chapter
+ *          EditChapter:
+ *              type: object
+ *              properties:
  *                  title:
  *                      type: string
  *                      example: chapter 1 of grief
@@ -43,6 +51,72 @@ router.put("/add" , AdminChapterController.addChapter)
  *              200:
  *                  description: success                
  */
+
+router.put("/add" , AdminChapterController.addChapter)
+
+/**
+ * @swagger
+ *  /admin/chapter/list/{videoID}:
+ *      get:
+ *          tags: [Chapter(AdminPanel)]
+ *          summary: get chapters of video
+ *          parameters:
+ *              -   in: path
+ *                  name: videoID
+ *                  type: string
+ *                  required: tue      
+ *          responses:
+ *              200:
+ *                  description: success                
+ */
+
+router.get("/list/:videoID" , AdminChapterController.getListOfChapters)
+
+/**
+ * @swagger
+ *  /admin/chapter/update/{chapterID}:
+ *      patch:
+ *          tags: [Chapter(AdminPanel)]
+ *          summary: Update details of chapter
+ *          parameters:
+ *              -   in: path
+ *                  name: chapterID
+ *                  type: string
+ *                  required: tue
+ *          requestBody:
+ *              required: true
+ *              content:
+ *                  application/x-www-form-urlencoded:
+ *                      schema:
+ *                          $ref: '#/components/schemas/EditChapter'
+ *                  application/json:
+ *                      schema:
+ *                          $ref: '#/components/schemas/EditChapter'      
+ *          responses:
+ *              200:
+ *                  description: success                
+ */
+
+router.patch("/update/:chapterID" , AdminChapterController.updateChapterById)
+
+/**
+ * @swagger
+ *  /admin/chapter/remove/{chapterID}:
+ *      patch:
+ *          tags: [Chapter(AdminPanel)]
+ *          summary: delete chapter of video by id
+ *          parameters:
+ *              -   in: path
+ *                  name: chapterID
+ *                  type: string
+ *                  required: tue      
+ *          responses:
+ *              200:
+ *                  description: success                
+ */
+
+router.patch("/remove/:chapterID" , AdminChapterController.removeChapterById)
+
 module.exports = {
     ChapterAdminApiRoutes : router
 }
