aa6ce59a87a6f6582096c3f954eda0c613332679
Fatemeh TayebiSalar <fatemehtayebi98@gmail.com>
2023-01-20 18:22:14 +0330

    the error was fixed

--- app/router/admin/event.js
+++ app/router/admin/event.js
@@ -1,13 +1,12 @@
 const { AdminEventController } = require("../../http/controllers/admin/event.controller");
-const { uploadFile } = require("../../utils/multer");
-
+const {uploadPicture} = require("../../utils/multer")
 const router = require("express").Router();
 
 
-router.post("/add" , uploadFile.single("coverImage") ,AdminEventController.addEvent)
+router.post("/add" , uploadPicture.single("coverImage") ,AdminEventController.addEvent)
 router.get("/" , AdminEventController.getListOfEvents)
 router.get("/:id", AdminEventController.getEventById);
-router.patch("/update/:id",uploadFile.single("coverImage"), AdminEventController.updateEventById)
+router.patch("/update/:id", uploadPicture.single("coverImage"), AdminEventController.updateEventById)
 router.delete("/remove/:id", AdminEventController.deleteEventById);
 
 
